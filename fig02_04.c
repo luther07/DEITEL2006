12999b02780c828b9d46bec2b9c9d0256839e111
Mark Johnson <luther07@gmail.com>
2011-05-27 03:55:16 -0500

    another very easy exercise

---
+++ fig02_04.c
@@ -0,0 +1,12 @@
+/* Fig 2.4: fig02_04.c
+	Printing multiple lines with a single printf */
+#include <stdio.h>
+
+/* function main begins program execution */
+int main( void )
+{
+   printf( "Welcome\nto\nC!\n " );
+
+   return 0; /* indicates that the program ended successfully */
+
+}  /* end function main */
