75098d4b8a7d0f17da45faeed6e94cc24ff40726
Mark Johnson <luther07@gmail.com>
2011-05-27 03:51:42 -0500

    add second very simple example

---
+++ fig02_03.c
@@ -0,0 +1,13 @@
+/* Fig 2.3: fig02_03.c
+	Printing on one line with two printf statements */
+#include <stdio.h>
+
+/* function main begins program execution */
+int main( void )
+{
+   printf( "Welcome " );
+   printf( "to C!\n" );
+
+   return 0; /* indicates that the program ended successfully */
+
+}  /* end function main */
