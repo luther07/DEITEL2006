166de9b9d42cfd25fd25306ee5099198c68e76ea
Mark Johnson <luther07@gmail.com>
2011-06-09 07:33:46 -0400

    counter-controlled repitition, You know what I'm saying.

---
+++ fig04_01.c
@@ -0,0 +1,17 @@
+/* Fig 4.1: fig04_01.c
+   Counter-controlled repitition	 */
+#include <stdio.h>
+
+/* function main begins program execution */
+int main( void )
+{
+   int counter = 1; /* loop counter initialization & initial value */
+
+   while( counter <= 10 ) { /* repitition condition */
+      printf( "%d\n", counter ); /* display counter */
+      ++counter; /* pre-increment loop counter */
+   } /* end while */   
+
+   return 0; /* indicates that the program ended successfully */
+
+}  /* end function main */
