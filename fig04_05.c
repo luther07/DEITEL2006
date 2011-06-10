48bbb02ed56f13620d9814b766b9fe3e1b9e4020
Mark Johnson <luther07@gmail.com>
2011-06-10 07:32:31 -0400

    summing even numbers, You know what I mean

---
+++ fig04_05.c
@@ -0,0 +1,21 @@
+/* Fig 4.5: fig04_05.c
+   Summation with for	 */
+#include <stdio.h>
+
+/* function main begins program execution */
+int main( void )
+{
+   int sum = 0; /* initialize sum */
+   int number;  /* number to be added to sum */
+
+   /*initialization, repitition condition, and increment
+     are included in the for statement header */
+   for( number = 2; number <= 100; number += 2 ) {
+      sum += number; /* add number to sum */
+   } /* end for */  
+
+   printf( "Sum is %d\n", sum ); /* output sum */
+
+   return 0; /* indicates that the program ended successfully */
+
+}  /* end function main */
