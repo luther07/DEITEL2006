3f95ee2da029c54147c6312399d471098a704b18
Mark Johnson <luther07@gmail.com>
2011-06-10 07:46:13 -0400

    use compile option, like cc -lm fig04_06.c, on Unix/Linux

---
+++ fig04_06.c
@@ -0,0 +1,29 @@
+/* Fig 4.6: fig04_06.c
+   Calculating compound interest	 */
+#include <stdio.h>
+#include <math.h>
+
+/* function main begins program execution */
+int main( void )
+{
+   double amount;              /* initialize amount */
+   double principal = 1000.0;  /* starting principal */
+   double rate = .05;          /* annual interest rate */
+   int year;                   /* year counter */
+
+   /* output table column head */
+   printf( "%4s%21s\n", "Year", "Amount on deposit" );
+
+   /* calculate amount on deposit for each of ten years */
+   for( year = 1; year <= 10; year++ ) {
+      
+   /* calculate new amount for specified year */
+   amount = principal * pow( 1.0 + rate, year );
+
+   /* output one table row */
+   printf( "%d%21.2f\n", year, amount );
+   } /* end for */  
+
+   return 0; /* indicates that the program ended successfully */
+
+}  /* end function main */
