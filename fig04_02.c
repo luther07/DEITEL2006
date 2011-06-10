4af323a67abec11e921b13d59dddd23c1f605191
Mark Johnson <luther07@gmail.com>
2011-06-10 07:17:00 -0400

    for loop, You know what I'm saying

---
+++ fig04_02.c
@@ -0,0 +1,18 @@
+/* Fig 4.2: fig04_02.c
+   Counter-controlled repitition with the for statement	 */
+#include <stdio.h>
+
+/* function main begins program execution */
+int main( void )
+{
+   int counter; /* loop counter initialization */
+
+   /*initialization, repitition condition, and increment
+     are included in the for statement header */
+   for( counter = 1; counter <= 10; counter++ ) {
+      printf( "%d\n", counter );
+   } /* end for */  
+
+   return 0; /* indicates that the program ended successfully */
+
+}  /* end function main */
