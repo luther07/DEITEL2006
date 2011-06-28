487e801d4aefb3b02227702c805bbe89bf382417
Mark Johnson <luther07@gmail.com>
2011-06-28 06:35:14 -0500

    do...while repitition

---
+++ fig04_09.c
@@ -0,0 +1,18 @@
+/* Fig 4.9: fig04_09.c
+   using the do/while repitition statement */
+#include <stdio.h>
+
+/* function main begins program execution */
+int main( void )
+{
+   int counter = 1; /* initialize counter */
+
+   do {
+      printf( "%d  ", counter ); /* display counter */
+   } while( ++counter <= 10); /* end do...while */
+
+   printf( "\n" ); /* newline character at the end of processing */
+
+   return 0; /* indicates that the program ended successfully */
+
+}  /* end function main */
