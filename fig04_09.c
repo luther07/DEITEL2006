a23e6a45b28944f4b2c5bb27426f3e123d323827
Mark Johnson <luther07@gmail.com>
2011-07-20 07:41:28 -0400

    add do while program

---
+++ fig04_09.c
@@ -0,0 +1,15 @@
+/* Fig 4.9: fig04_09.c
+	Using the do/while repitition statement */
+#include <stdio.h>
+
+/* function main begins program execution */
+int main( void )
+{
+   int counter = 1; /* initialize counter */
+
+   do {
+      printf( "%d ", counter ); /* display counter */
+   } while ( ++counter <= 10 ); /* end do...while */
+   return 0; /* indicates that the program ended successfully */
+
+}  /* end function main */
