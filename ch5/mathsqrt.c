30331d46f79f7ad670aa6487e307130ef4769514
Mark Johnson <luther07@gmail.com>
2011-07-21 19:38:35 -0400

    sqrt function

---
+++ ch5/mathsqrt.c
@@ -0,0 +1,12 @@
+/* stdfun.c
+	Testing sqrt function which returns doubles. */
+#include <stdio.h>
+#include <math.h>
+
+/* function main begins program execution */
+int main( void )
+{
+   printf( "%.1f\n", sqrt( 900.0 ) );
+   return 0; /* indicates that the program ended successfully */
+
+}  /* end function main */
