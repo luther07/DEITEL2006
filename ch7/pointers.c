5e5c4584612de0f857cdba2e6a1591c2d2301d67
Mark Johnson <luther07@gmail.com>
2012-01-15 17:41:17 -0600

    sizeof function exercises

--- ch7/pointers.c
+++ ch7/pointers.c
@@ -9,9 +9,32 @@ int main( void )
    int *yPtr = &y;
 
    printf( "Welcome to C!\n" );
-   printf( "Integer y equals %d\n" , y); /* print y */
-   printf( "Pointer yPtr points to address: %p\n", yPtr); /* print the hex address stored in variable yPtr */
-   printf( "The value stored at yPtr's address is: %d\n", *yPtr); /* print value stored in the address that yPtr contains */
+   printf( "Integer y equals %d\n" , y ); /* print y */
+   printf( "Pointer yPtr points to address: %p\n", yPtr ); /* print the hex address stored in variable yPtr */
+   printf( "The value stored at yPtr's address is: %d\n", *yPtr ); /* print value stored in the address that yPtr contains */
+   printf( "Change value held by y to 7...\n" ); /* output */
+
+   y = 7;
+
+   printf( "Integer y equals %d\n", y ); /* print y */
+   printf( "Pointer yPtr points to address: %p\n", yPtr ); /* print the hex address stored in variable yPtr */
+   printf( "The value stored at yPtr's address is: %d\n", *yPtr ); /* print value stored in the address that yPtr contains */
+
+   printf( "What does it look like when you initialize a pointer to 0?\n" ); /* output */
+
+   int *zeroPtr = 0; /* initialize pointer to 0 */
+
+   printf( "Pointer zeroPtr points to address: %p\n", zeroPtr ); /* print the hex address stored in variable zeroPtr */
+   // printf( "The value stored at zeroPtr's address is: %d\n", *zeroPtr ); /* seg fault */
+   printf( "Trying to dereference a NULL pointer causes seg fault. Initializing to 0 is equivalent to initializing a pointer to NULL.\n" );
+
+   printf( "What does it look like when you initialize a pointer to NULL?\n" ); /* output */
+
+   int *nullPtr = NULL; /* initialize pointer to NULL */
+
+   printf( "Pointer nullPtr points to address: %p\n", nullPtr ); /* print the hex address stored in variable zeropointer */
+   // printf( "The value stored at nullPtr's address is: %d\n", *nullPtr ); /* seg fault */
+   printf( "Trying to dereference a NULL pointer causes a seg fault.\n" );
 
    return 0; /* indicates that the program ended successfully */
 
